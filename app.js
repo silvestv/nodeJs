d5c421a624146e3c5e46aa583d9e84944d3eba8e
SILVESTRE Victor <victor.silvestre@soprasteria.com>
2021-12-17 01:43:05 +0100

    add put, delete methods - simple CRUD app finished

--- app.js
+++ app.js
@@ -38,6 +38,19 @@ app.use((req, res, next) => {
     next();
 })
 
+//Middleware PUT
+app.put('/api/stuff/:id', (req, res, next) => {
+    Thing.updateOne({ _id: req.params.id}, {...req.body, _id: req.params.id})
+    .then(_ => res.status(200).json({ message: 'Objet modifié ! '}))
+    .catch(error => res.status(400).json({error}));
+});
+
+app.delete('/api/stuff/:id', (req, res, next) =>{
+    Thing.deleteOne({_id: req.params.id})
+    .then(_ => res.status(200).json({message: 'Objet supprimé'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
 // Middleware POST
 app.post('/api/stuff', (req, res, next) => {
     // Nous n'avons accès au body de request, uniquement grâce à use(express.json)
